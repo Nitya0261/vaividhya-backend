d06a145359e094e13c0f7414a8cc86d4cb96f853
Nitya Beladiya <nitya0261@Nityas-MacBook-Air.local>
2026-01-22 22:50:56 +0530

    remove cors

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ app = FastAPI(
 # ===================== CORS =====================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://your-frontend.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
